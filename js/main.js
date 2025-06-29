e4f23198ccb1b856eb8914b22e0b6ce65e3a579c
FoxyDevSolution <foxydevsolution@gmail.com>
2025-06-29 00:55:49 -0300

    ajustes de diseño y terminación de la sección de entradas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const splash = document.getElementById("splash-screen");
+  const menuPrincipal = document.getElementById("menu-principal");
+
+  const desdeOtraPagina = sessionStorage.getItem("desdeOtraPagina");
+
+  if (desdeOtraPagina === "true") {
+    splash.style.display = "none";
+    menuPrincipal.style.display = "block";
+    sessionStorage.removeItem("desdeOtraPagina");
+  } else {
+    const btn = document.getElementById("enter-btn");
+    btn.addEventListener("click", () => {
+      splash.style.display = "none";
+      menuPrincipal.style.display = "block";
+    });
+  }
+});
+
+
+
+/*
+document.addEventListener("DOMContentLoaded", () => {
   const btn = document.getElementById("enter-btn");
   const splash = document.getElementById("splash-screen");
   const menuPrincipal = document.getElementById("menu-principal");
@@ -8,3 +30,4 @@ document.addEventListener("DOMContentLoaded", () => {
     menuPrincipal.style.display = "block";
   });
 });
+*/
