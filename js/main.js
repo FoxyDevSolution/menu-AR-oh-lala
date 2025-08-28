a76e3b3a8d66e54c9dc6224cc9e456c091e6fc7a
FoxyDevSolution <foxydevsolution@gmail.com>
2025-08-27 22:31:32 -0300

    Saca la musica para mejorar la experiencia de usuario

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   const splash = document.getElementById("splash-screen");
   const menuPrincipal = document.getElementById("menu-principal");
+
+  const desdeOtraPagina = sessionStorage.getItem("desdeOtraPagina");
+
+  if (desdeOtraPagina === "true") {
+    splash.style.display = "none";
+    menuPrincipal.style.display = "block";
+    sessionStorage.removeItem("desdeOtraPagina");
+  } else {
+    const btn = document.getElementById("enter-btn");
+    btn.addEventListener("click", () => {
+      splash.style.display = "none";
+      menuPrincipal.style.display = "block";
+    });
+  }
+});
+
+
+
+/*
+
+document.addEventListener("DOMContentLoaded", () => {
+  const splash = document.getElementById("splash-screen");
+  const menuPrincipal = document.getElementById("menu-principal");
   const enterBtn = document.getElementById("enter-btn");
   const audio = document.getElementById("welcome-audio");
 
@@ -37,7 +60,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-
+*/
 
 
 
